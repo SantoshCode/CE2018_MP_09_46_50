8ae9887cee8846b952017d98f7b628018603e531
Santosh Subedi <50507577+SantoshCode@users.noreply.github.com>
2020-02-17 20:47:21 +0545

    update new chart added

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 var xDataChart1 = [0];
 var yDataChart1 = [0];
 
@@ -9,110 +7,14 @@ var yDataChart2 = [0];
 var unsorArray = [];
 var sortedArr = [];
 
-//==================================================================================
-
-// function mergeSort(unsortedArray) {
-//   // No need to sort the array if the array only has one element or empty
-//   if (unsortedArray.length <= 1) {
-//     return unsortedArray;
-//   }
-//   // In order to divide the array in half, we need to figure out the middle
-//   const middle = Math.floor(unsortedArray.length / 2);
-
-//   // This is where we will be dividing the array into left and right
-//   const left = unsortedArray.slice(0, middle);
-//   const right = unsortedArray.slice(middle);
-
-//   // Using recursion to combine the left and right
-//   return merge(mergeSort(left), mergeSort(right));
-// }
-
-// function merge(left, right) {
-//   let resultArray = [],
-//     leftIndex = 0,
-//     rightIndex = 0;
-
-//   // We will concatenate values into the resultArray in order
-//   while (leftIndex < left.length && rightIndex < right.length) {
-//     if (left[leftIndex] < right[rightIndex]) {
-//       resultArray.push(left[leftIndex]);
-//       leftIndex++; // move left array cursor
-//     } else {
-//       resultArray.push(right[rightIndex]);
-//       rightIndex++; // move right array cursor
-//     }
-//   }
-//   // We need to concat here because there will be one element remaining
-//   // from either left OR the right
-//   return resultArray
-//     .concat(left.slice(leftIndex))
-//     .concat(right.slice(rightIndex));
-// }
-
-// let selectionSort = arr => {
-//   let len = arr.length;
-//   for (let i = 0; i < len; i++) {
-//     let min = i;
-//     for (let j = i + 1; j < len; j++) {
-//       if (arr[min] > arr[j]) {
-//         min = j;
-//       }
-//     }
-//     if (min !== i) {
-//       let tmp = arr[i];
-//       arr[i] = arr[min];
-//       arr[min] = tmp;
-//     }
-//   }
-//   return arr;
-// };
-
-//==============================================================================================
-
-// document.getElementById("bestBtn").addEventListener("click", () => {
-//   var inputVal = document.getElementById("target").value;
-//   console.log(inputVal);
-
-//   for (let i = 0; i < inputVal; i++) {
-//     unsorArray[i] = i;
-//   }
-//   document.getElementById(
-//     "gen"
-//   ).innerText = `Generating sorted array of size ${inputVal} `;
-
-//   document.getElementById(
-//     "arr"
-//   ).innerText = `Generated array is ${unsorArray} `;
-// });
-
-//==============================================================================================
-
-// document.getElementById("worstBtn").addEventListener("click", () => {
-//   var inputVal = document.getElementById("target").value;
-//   console.log(inputVal);
-
-//   for (let i = 0; i < inputVal; i++) {
-//     unsorArray[i] = inputVal - i;
-//   }
-//   document.getElementById(
-//     "gen"
-//   ).innerText = `Generating worst case array of size ${inputVal} `;
-
-//   document.getElementById(
-//     "arr"
-//   ).innerText = `Generated array is ${unsorArray} `;
-// });
-
 //==============================================================================================
 
 document.getElementById("randomBtn").addEventListener("click", () => {
   var inputVal = document.getElementById("target").value;
   console.log(inputVal);
+  
+  unsorArray = Array.apply(null, {length:inputVal}).map(()=>(Math.random() * (100 - 10) + 10).toFixed(2))
 
-  for (let i = 0; i < inputVal; i++) {
-    unsorArray[i] = (Math.random() * (100 - 10) + 10).toFixed(2);
-  }
-  unsortArray = Array.apply(null, {length:inputVal}).map(()=>(Math.random() * (100 - 10) + 10).toFixed(2))
   document.getElementById(
     "gen"
   ).innerText = `Generating random case array of size ${inputVal} `;
@@ -134,102 +36,32 @@ document.getElementById("btn").addEventListener("click", () => {
   var t2 = performance.now();
 
   document.getElementById("sort").innerText = `Sorted array is ${sortedArr} `;
-
+  sortedArr = []
   xDataChart1.push(inputVal);
-  yDataChart1.push(t2 - t1);
+  yDataChart1.push((t2 - t1)*5);
 
   var data = [
     {
       x: xDataChart1,
       y: yDataChart1,
-      mode: "markers+lines",
-      // type: "line",
-      type: "scatter",
-      // name: 'Spline Shape (1.3)',
+      type: 'scatter',
       line: {
-        shape: "linear",
-        smoothing: 1.3,
-        color: "rgb(114, 94, 242)"
+        color: "rgb(0, 255, 0)"
       }
     }
   ];
 
   var layout = {
-    title: "Merg Sort",
-    autosize: false,
-    width: 500,
-    height: 500,
-    margin: {
-      l: 50,
-      r: 50,
-      b: 100,
-      t: 100,
-      pad: 4
-    },
-    font: {
-      family: "Lato",
-      size: 16,
-      color: "rgb(100,150,200)"
-    },
-    plot_bgcolor: "rgba(200,255,0,0.1)",
-    margin: {
-      pad: 10
-    },
-    xaxis: {
-      title: "n(Size of array)",
-      titlefont: {
-        color: "black",
-        size: 12
-      },
-      rangemode: "tozero"
-    },
-    yaxis: {
-      title: "Time elapsed in millisecond",
-      titlefont: {
-        color: "black",
-        size: 12
-      },
-      rangemode: "tozero"
-    }
+    title:"Merge Sort",
+    xaxis: {title: 'Size of the array'},
+    yaxis: {title: 'Milliseconds Took'},
+    
   };
 
   var layout2 = {
-    title: "Selection Sort",
-    autosize: false,
-    width: 500,
-    height: 500,
-    margin: {
-      l: 50,
-      r: 50,
-      b: 100,
-      t: 100,
-      pad: 4
-    },
-    font: {
-      family: "Lato",
-      size: 16,
-      color: "rgb(100,150,200)"
-    },
-    plot_bgcolor: "rgba(200,255,0,0.1)",
-    margin: {
-      pad: 10
-    },
-    xaxis: {
-      title: "n(Size of array)",
-      titlefont: {
-        color: "black",
-        size: 12
-      },
-      rangemode: "tozero"
-    },
-    yaxis: {
-      title: "Time elapsed in millisecond",
-      titlefont: {
-        color: "black",
-        size: 12
-      },
-      rangemode: "tozero"
-    }
+    title:"Selection Sort",
+    xaxis: {title: 'Size of the array'},
+    yaxis: {title: 'Milliseconds Took'},
   };
   Plotly.plot("chart1", data, layout);
 
@@ -238,23 +70,42 @@ document.getElementById("btn").addEventListener("click", () => {
   var t2chart2 = performance.now();
 
   xDataChart2.push(inputVal);
-  yDataChart2.push(t2chart2 - t1chart2);
+  yDataChart2.push((t2chart2 - t1chart2)*5);
 
   var data2 = [
     {
       x: xDataChart2,
       y: yDataChart2,
-      mode: "markers+lines",
-      // type: "line",
-      type: "scatter",
-      // name: 'Spline Shape (1.3)',
+      type: 'scatter',
       line: {
-        shape: "linear",
-        smoothing: 1.3,
         color: "rgb(255, 0, 0)"
       }
     }
   ];
-
+  var layout3 = {
+    title:"Merge and Selection Sort",
+    xaxis: {title: 'Size of the array'},
+    yaxis: {title: 'Milliseconds Took'},
+  };
+  var combinedData = [
+    {
+      x: xDataChart1,
+      y: yDataChart1,
+      type: 'scatter',
+      line: {
+        color: "rgb(0, 255, 0)"
+      }
+    },
+    {
+      x: xDataChart2,
+      y: yDataChart2,
+      type: 'scatter',
+      line: {
+        color: "rgb(255, 0, 0)"
+      }
+    }
+  ]
   Plotly.plot("chart2", data2, layout2);
+  Plotly.plot("chart3", combinedData, layout3);
+
 });
